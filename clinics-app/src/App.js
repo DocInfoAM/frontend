d26a6c2339155bd78ed1a54f9f6454c3147cdd08
aemorozov <admin@aemorozov.com>
2023-07-20 14:46:27 +0400

    add simple structure

--- clinics-app/src/App.js
+++ clinics-app/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className="header">
+        <div className="div-for-header-img">
+          <img id="logo" alt="logo" title="logo" style={{ width: 60 }} src="https://webstockreview.net/images/patient-clipart-patient-identification-19.png" />
+        </div>
+        <nav className="nav">
+          <div><a id="point1" className="point" href="##">Point 1</a></div>
+          <div><a id="point2" className="point" href="##">Point 2</a></div>
+          <div><a id="point3" className="point" href="##">Point 3</a></div>
+        </nav>
+        <div id="change-languages" className="change-languages">
+          <p>Флажок языка</p>
+        </div>
       </header>
     </div>
   );
---
+++ clinics-app/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <header>
+            <ul>
+                <li><Link to="##">Main</Link></li>
+                <li><a href="##">Point 2</a></li>
+                <li><a href="##">Point 3</a></li>
+                <li><a href="##">Point 4</a></li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header 
