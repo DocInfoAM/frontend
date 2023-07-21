7b796d2d5bb3627930600fada3471f91a467970c
aemorozov <admin@aemorozov.com>
2023-07-21 14:22:58 +0400

    add fixed header

--- clinics-app/src/components/ChangeLanguages.jsx
+++ clinics-app/src/components/ChangeLanguages.jsx
@@ -4,9 +4,9 @@ const ChangeLanguages = () => {
     return (
         <div className={classes.change_languages}>
           <p>
-            <span className={classes.flag_icon + ' ' + classes.flag_icon_arm} id="arm"></span>
+            <span className={`${classes.flag_icon} ${classes.flag_icon_arm}`} id="arm"></span>
             {/* <span className="flag_icon flag_icon_rus" id="rus"></span> */}
-            <span className={classes.flag_icon + ' ' + classes.flag_icon_gbr} id="gbr"></span>
+            <span className={`${classes.flag_icon} ${classes.flag_icon_gbr}`} id="gbr"></span>
           </p>
         </div>
     )
