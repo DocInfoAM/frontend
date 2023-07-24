c748624d79d77ce0cb7359ddcdc85846355e955f
aemorozov <admin@aemorozov.com>
2023-07-24 19:26:40 +0400

    fix some code

--- clinics-app/src/App.js
+++ clinics-app/src/App.js
@@ -1,35 +1,35 @@
-import './global.css'
-import Header from './components/Header/Header'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import MyClinic from './pages/MyClinic/MyClinic'
-import Claims from './pages/Claims/Claims'
-import Schedule from './pages/Schedule/Schedule'
-import PersonalArea from './pages/PersonalArea/PersonalArea'
-import AllDoctors from './pages/AllDoctors/AllDoctors'
-import Contacts from './pages/Contacts/Contacts'
-import SignOut from './pages/SignOut/SignOut'
-import mainScreen from './pages'
+import "./App.css";
+import Header from "./components/Header/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MyClinic from "./pages/MyClinic/MyClinic";
+import Claims from "./pages/Claims/Claims";
+import Schedule from "./pages/Schedule/Schedule";
+import PersonalArea from "./pages/PersonalArea/PersonalArea";
+import AllDoctors from "./pages/AllDoctors/AllDoctors";
+import Contacts from "./pages/Contacts/Contacts";
+import SignOut from "./pages/SignOut/SignOut";
+import mainScreen from "./pages";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <div className="app-wrapper">
-                <Header />
-                <div className="content">
-                    <Routes>
-                        <Route path="/" Component={mainScreen} />
-                        <Route path="/myclinic" Component={MyClinic} />
-                        <Route path="/claims" Component={Claims} />
-                        <Route path="/schedule" Component={Schedule} />
-                        <Route path="/personalarea" Component={PersonalArea} />
-                        <Route path="/alldoctors" Component={AllDoctors} />
-                        <Route path="/contacts" Component={Contacts} />
-                        <Route path="/signout" Component={SignOut} />
-                    </Routes>
-                </div>
-            </div>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" Component={mainScreen} />
+            <Route path="/myclinic" Component={MyClinic} />
+            <Route path="/claims" Component={Claims} />
+            <Route path="/schedule" Component={Schedule} />
+            <Route path="/personalarea" Component={PersonalArea} />
+            <Route path="/alldoctors" Component={AllDoctors} />
+            <Route path="/contacts" Component={Contacts} />
+            <Route path="/signout" Component={SignOut} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- clinics-app/src/components/Header/Navigation/Navigation.jsx
+++ clinics-app/src/components/Header/Navigation/Navigation.jsx
@@ -4,13 +4,27 @@ import { Link } from "react-router-dom";
 const Navigation = () => {
   return (
     <nav className={classes.nav}>
-      <Link to="/myclinic"><div className={classes.item}>Моя клиника</div></Link>
-      <Link to="/claims"><div className={classes.item}>Заявки</div></Link>
-      <Link to="/schedule"><div className={classes.item}>Расписание</div></Link>
-      <Link to="/personalarea"><div className={classes.item}>Личный кабинет врача</div></Link>
-      <Link to="/alldoctors"><div className={classes.item}>Все врачи</div></Link>
-      <Link to="/contacts"><div className={classes.item}>Контакты</div></Link>
-      <Link to="/signout"><div className={classes.item}>Выход</div></Link>
+      <Link to="/myclinic">
+        <div className={classes.item}>Моя клиника</div>
+      </Link>
+      <Link to="/claims">
+        <div className={classes.item}>Заявки</div>
+      </Link>
+      <Link to="/schedule">
+        <div className={classes.item}>Расписание</div>
+      </Link>
+      <Link to="/personalarea">
+        <div className={classes.item}>Личный кабинет врача</div>
+      </Link>
+      <Link to="/alldoctors">
+        <div className={classes.item}>Все врачи</div>
+      </Link>
+      <Link to="/contacts">
+        <div className={classes.item}>Контакты</div>
+      </Link>
+      <Link to="/signout">
+        <div className={classes.item}>Выход</div>
+      </Link>
     </nav>
   );
 };
