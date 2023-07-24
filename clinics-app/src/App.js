37bfb4d31d04fd1121da5807057fe4861812dc0d
aemorozov <admin@aemorozov.com>
2023-07-24 13:02:54 +0400

    add pages

--- clinics-app/src/App.js
+++ clinics-app/src/App.js
@@ -1,12 +1,19 @@
-import './global.css';
-import Header from './components/Header/Header';
+import './global.css'
+import Header from './components/Header/Header'
+import MyClinic from './components/MyClinic/MyClinic'
+import Claims from './components/Claims/Claims'
 
 function App() {
-  return (
-    <div className="app-wrapper">
-      <Header />
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <Header />
+            <div className="content">
+                
+                <Claims />
+                <MyClinic />
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
