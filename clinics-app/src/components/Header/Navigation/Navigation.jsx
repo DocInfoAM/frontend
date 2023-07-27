d74f14963a01dba89173fa5128612581eed7aafa
aemorozov <admin@aemorozov.com>
2023-07-27 14:43:00 +0400

    add setActive function

--- clinics-app/src/components/Header/Navigation/Navigation.jsx
+++ clinics-app/src/components/Header/Navigation/Navigation.jsx
@@ -4,29 +4,31 @@ import { NavLink } from "react-router-dom";
 const Navigation = () => {
   return (
     <nav className={classes.nav}>
-      <NavLink to="/my-clinic" id="MyClinic" className={({ isActive }) => (isActive ? classes.active : "")}>
+      <NavLink to="/my-clinic" id="MyClinic" className={setActive}>
         <div className={classes.item}>Моя клиника</div>
       </NavLink>
-      <NavLink to="/claims" id="Claims" className={({ isActive }) => (isActive ? classes.active : "")}>
+      <NavLink to="/claims" id="Claims" className={setActive}>
         <div className={classes.item}>Заявки</div>
       </NavLink>
-      <NavLink to="/schedule" id="Schedule" className={({ isActive }) => (isActive ? classes.active : "")}>
+      <NavLink to="/schedule" id="Schedule" className={setActive}>
         <div className={classes.item}>Расписание врача</div>
       </NavLink>
-      <NavLink to="/personal-area" id="PersonalArea" className={({ isActive }) => (isActive ? classes.active : "")}>
+      <NavLink to="/personal-area" id="PersonalArea" className={setActive}>
         <div className={classes.item}>Личный кабинет врача</div>
       </NavLink>
-      <NavLink to="/all-doctors" id="AllDoctors" className={({ isActive }) => (isActive ? classes.active : "")}>
+      <NavLink to="/all-doctors" id="AllDoctors" className={setActive}>
         <div className={classes.item}>Все врачи</div>
       </NavLink>
-      <NavLink to="/contacts" id="Contacts" className={({ isActive }) => (isActive ? classes.active : "")}>
+      <NavLink to="/contacts" id="Contacts" className={setActive}>
         <div className={classes.item}>Контакты</div>
       </NavLink>
-      <NavLink to="/sign-out" id="SignOut" className={({ isActive }) => (isActive ? classes.active : "")}>
+      <NavLink to="/sign-out" id="SignOut" className={setActive}>
         <div className={classes.item}>Выход</div>
       </NavLink>
     </nav>
   );
 };
+
+const setActive = ({ isActive }) => (isActive ? classes.active : "")
 
 export default Navigation;
