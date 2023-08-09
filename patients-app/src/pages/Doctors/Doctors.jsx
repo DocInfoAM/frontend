e769e44d04cced2892c95f0b810cc7e9264e5b20
Kerim <gossinummoss@gmail.com>
2023-08-09 18:07:32 +0400

    update Doctors.jsx

--- patients-app/src/pages/Doctors/Doctors.jsx
+++ patients-app/src/pages/Doctors/Doctors.jsx
@@ -1,6 +1,22 @@
+const getDoctors = async () => {
+    const res = await fetch(
+      "https://mock-server-docinfo.onrender.com/doctors"
+     );
+   return await res.json();
+   };
+   
+    const doctorList = await getDoctors()
+    console.log(doctorList)
+
 const Doctors = () => {
     return (
-        <div>доктора</div>
+        <>
+        <p>результаты поиска:</p>
+        {
+
+        }
+        </>
+
     )
 }
 
